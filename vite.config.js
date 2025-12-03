29997a681ad38ee95b4fb3449a07311211133a9a
Elijah Buckwalter <elijahbuckwalter@gmail.com>
2025-12-02 17:57:54 -0700

    Adjusting the vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
+        'ws': {
+          target: 'ws://localhost:3000',
+          ws: true,
+        },
         changeOrigin: true,
       },
     },
