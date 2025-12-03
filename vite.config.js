5d2fb0ebe583b6ad93f9219b369c7363f09efd81
Elijah Buckwalter <elijahbuckwalter@gmail.com>
2025-12-03 10:01:22 -0700

    I think everything is working

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        'ws': {
-          target: 'ws://localhost:3000',
-          ws: true,
-        },
-        changeOrigin: true,
+      '/api': 'http://localhost:3000',
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
       },
     },
   },
